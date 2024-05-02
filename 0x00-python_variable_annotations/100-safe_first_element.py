9be79d3701075877ea9e409cc9d6bface44e4cf1
abdiwoli <abdiwolih@gmail.com>
2024-05-02 12:26:42 +0300

    annotation

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -2,10 +2,8 @@
 """ advanced tasks """
 from typing import Iterable, Sequence, List, Tuple, Any, Union
 
-NoneType = type(None)
 
-
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """ add anation to paramaters"""
     if lst:
         return lst[0]
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """ advanced tasks """
-from typing import Iterable, Sequence, List, Tuple, Any, Union, Mapping, TypeVar
+from typing import Iterable, Sequence
+from typing import List, Tuple, Any, Union, Mapping, TypeVar
 
 
 def safely_get_value(dct: Mapping, key: Any, default: Union[TypeVar(
         'T'), None] = None) -> Union[TypeVar('T'), type(None)]:
+    """ function saveley get value """
     if key in dct:
         return dct[key]
     else:
