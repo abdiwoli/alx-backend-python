f53e5cc829a6d9bf4df5919f8597e9b6f0f54981
=abdiwoli <=>
2024-08-27 21:51:57 +0300

    measure

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+""" module documentation """
 import asyncio
 import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
+
 
 def measure_time(n: int, max_delay: int) -> float:
     start = time.time()
